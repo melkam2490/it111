be12b2b3e08119ca8a0e6ae19dbedac805aada79
melkam2490 <129891028+melkam2490@users.noreply.github.com>
2023-04-21 18:50:53 -0700

    Add files via upload
    week1 folder is added to the repo

---
+++ week1/src/MyVariable1.java
@@ -0,0 +1,45 @@
+public class MyVariable1{
+
+    public static void main(String[] args){
+
+        String name = "Olga";
+        int hours =40;
+
+        System.out.println(name);
+        System.out.print(hours);
+
+        String building,tree,lake,ocean;
+
+               tree = " Redwood";
+            building ="Empire State";
+            lake ="Lake Superior";
+            ocean ="Pacific Ocean";
+//        String building = "Empire State" , tree = "Redwood" , lake ="Lake Superior", ocean= "pacific";
+//
+        int weeks,decades, months ,years;
+        weeks = 4;
+        decades = 10;
+        months =3;
+      years = 5;
+
+//       int weeks = 4, decades = 10, months = 3, years =5;
+
+        System.out.println(tree);
+        System.out.println(building);
+        System.out.println(lake);
+        System.out.println(ocean);
+        System.out.println(weeks);
+        System.out.println(decades);
+        System.out.println(months);
+        System.out.println(years);
+
+
+
+
+    }
+
+
+}
+
+
+
